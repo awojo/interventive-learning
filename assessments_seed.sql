88eed69af58f3f2708ac6e3f464549b01d920c83
Riya Jain <riya.p.jain@wmich.edu>
2026-04-20 08:19:49 -0400

    Updated Assessment

--- assessments_seed.sql
+++ assessments_seed.sql
@@ -34,7 +34,6 @@ WHERE
         FROM a
     );
 
-<<<<<<< HEAD
 INSERT INTO
     assessment_questions (
         assessment_id,
@@ -97,25 +96,6 @@ VALUES (
         'B',
         5
     );
-=======
-INSERT INTO assessment_questions (
-    assessment_id,
-    question,
-    answer,
-    sequence_index
-)
-VALUES
-((SELECT id FROM assessments WHERE item_code='K.RSLKID.1.A'),
-'Who did Megan meet? [MCQ: A) Heather | B) Max | C) Miss Bye | D) Happy]', 'A', 1),
-((SELECT id FROM assessments WHERE item_code='K.RSLKID.1.A'),
-'[TF] Heather has a puppy named Max.', 'T', 2),
-((SELECT id FROM assessments WHERE item_code='K.RSLKID.1.A'),
-'Where did Megan sit? [MCQ: A) At school | B) On bus | C) At home | D) Park]', 'A', 3),
-((SELECT id FROM assessments WHERE item_code='K.RSLKID.1.A'),
-'[TF] Did Megan have a good day?', 'F', 4),
-((SELECT id FROM assessments WHERE item_code='K.RSLKID.1.A'),
-'What did Megan say? [MCQ: A) I am tired | B) I made a new friend! | C) I lost my puppy | D) I am sad]', 'B', 5);
->>>>>>> 5ab6d86470e9249873a8b1f1b4dfdecdd344007d
 
 -- GRADE 1
 INSERT INTO
@@ -151,7 +131,6 @@ WHERE
         FROM a
     );
 
-<<<<<<< HEAD
 INSERT INTO
     assessment_questions (
         assessment_id,
@@ -214,25 +193,6 @@ VALUES (
         'B',
         5
     );
-=======
-INSERT INTO assessment_questions (
-    assessment_id,
-    question,
-    answer,
-    sequence_index
-)
-VALUES
-((SELECT id FROM assessments WHERE item_code='1.RSLKID.1.A'),
-'Who did Megan meet on the bus? [MCQ: A) Heather | B) Max | C) Mom | D) Miss Bye]', 'A', 1),
-((SELECT id FROM assessments WHERE item_code='1.RSLKID.1.A'),
-'[TF] Heather has a puppy named Max.', 'T', 2),
-((SELECT id FROM assessments WHERE item_code='1.RSLKID.1.A'),
-'Where did they sit together? [MCQ: A) Bus | B) School | C) Home | D) Park]', 'B', 3),
-((SELECT id FROM assessments WHERE item_code='1.RSLKID.1.A'),
-'[TF] Megan did not have a good day.', 'F', 4),
-((SELECT id FROM assessments WHERE item_code='1.RSLKID.1.A'),
-'What did Megan say at the end? [MCQ: A) I am lost | B) I made a new friend! | C) I am tired | D) I cried]', 'B', 5);
->>>>>>> 5ab6d86470e9249873a8b1f1b4dfdecdd344007d
 
 -- GRADE 2
 INSERT INTO
@@ -268,7 +228,6 @@ WHERE
         FROM a
     );
 
-<<<<<<< HEAD
 INSERT INTO
     assessment_questions (
         assessment_id,
@@ -331,25 +290,6 @@ VALUES (
         'false | F | f',
         5
     );
-=======
-INSERT INTO assessment_questions (
-    assessment_id,
-    question,
-    answer,
-    sequence_index
-)
-VALUES
-((SELECT id FROM assessments WHERE item_code='2.RSLKID.1.A'),
-'Who did Megan meet on the bus? [MCQ: A) Heather | B) Teacher | C) Mom | D) Happy]', 'A', 1),
-((SELECT id FROM assessments WHERE item_code='2.RSLKID.1.A'),
-'What is Heather’s puppy’s name? [MCQ: A) Max | B) Happy | C) Buddy | D) Spot]', 'B', 2),
-((SELECT id FROM assessments WHERE item_code='2.RSLKID.1.A'),
-'[TF] Megan and Heather sat together.', 'T', 3),
-((SELECT id FROM assessments WHERE item_code='2.RSLKID.1.A'),
-'Where did they eat? [MCQ: A) Bus | B) At lunch | C) Home | D) Playground]', 'B', 4),
-((SELECT id FROM assessments WHERE item_code='2.RSLKID.1.A'),
-'[TF] Megan said her day was not good.', 'F', 5);
->>>>>>> 5ab6d86470e9249873a8b1f1b4dfdecdd344007d
 
 -- GRADE 3
 INSERT INTO
@@ -385,7 +325,6 @@ WHERE
         FROM a
     );
 
-<<<<<<< HEAD
 INSERT INTO
     assessment_questions (
         assessment_id,
@@ -448,25 +387,6 @@ VALUES (
         'C',
         5
     );
-=======
-INSERT INTO assessment_questions (
-    assessment_id,
-    question,
-    answer,
-    sequence_index
-)
-VALUES
-((SELECT id FROM assessments WHERE item_code='3.RSLKID.1.A'),
-'Who did Megan meet on the bus? [MCQ: A) Heather | B) Miss Bye | C) Mom | D) Happy]', 'A', 1),
-((SELECT id FROM assessments WHERE item_code='3.RSLKID.1.A'),
-'[TF] Megan and Heather both have puppies.', 'T', 2),
-((SELECT id FROM assessments WHERE item_code='3.RSLKID.1.A'),
-'What is Megan’s puppy’s name? [MCQ: A) Max | B) Buddy | C) Happy | D) Spot]', 'C', 3),
-((SELECT id FROM assessments WHERE item_code='3.RSLKID.1.A'),
-'[TF] Megan did not like her teacher.', 'F', 4),
-((SELECT id FROM assessments WHERE item_code='3.RSLKID.1.A'),
-'What did Megan say? [MCQ: A) It was boring | B) It was scary | C) It was the best first day ever! | D) I am sad]', 'C', 5);
->>>>>>> 5ab6d86470e9249873a8b1f1b4dfdecdd344007d
 
 -- GRADE 4
 INSERT INTO
@@ -502,7 +422,6 @@ WHERE
         FROM a
     );
 
-<<<<<<< HEAD
 INSERT INTO
     assessment_questions (
         assessment_id,
@@ -565,25 +484,6 @@ VALUES (
         'C',
         5
     );
-=======
-INSERT INTO assessment_questions (
-    assessment_id,
-    question,
-    answer,
-    sequence_index
-)
-VALUES
-((SELECT id FROM assessments WHERE item_code='4.RSLKID.1.A'),
-'Which detail shows Megan was nervous? [MCQ: A) Smiling | B) Butterflies in her stomach | C) Singing | D) Laughing]', 'B', 1),
-((SELECT id FROM assessments WHERE item_code='4.RSLKID.1.A'),
-'[TF] Megan and Heather met for the first time at school.', 'F', 2),
-((SELECT id FROM assessments WHERE item_code='4.RSLKID.1.A'),
-'What can you infer? [MCQ: A) They hate each other | B) They become friends quickly | C) They ignore each other | D) They fight]', 'B', 3),
-((SELECT id FROM assessments WHERE item_code='4.RSLKID.1.A'),
-'[TF] Megan enjoyed her first day.', 'T', 4),
-((SELECT id FROM assessments WHERE item_code='4.RSLKID.1.A'),
-'Which detail shows happiness? [MCQ: A) Crying | B) Running away | C) She smiled all the way | D) Sleeping]', 'C', 5);
->>>>>>> 5ab6d86470e9249873a8b1f1b4dfdecdd344007d
 
 -- GRADE 5
 INSERT INTO
@@ -619,7 +519,6 @@ WHERE
         FROM a
     );
 
-<<<<<<< HEAD
 INSERT INTO
     assessment_questions (
         assessment_id,
@@ -682,24 +581,5 @@ VALUES (
         'C',
         5
     );
-=======
-INSERT INTO assessment_questions (
-    assessment_id,
-    question,
-    answer,
-    sequence_index
-)
-VALUES
-((SELECT id FROM assessments WHERE item_code='5.RSLKID.1.A'),
-'Which quote shows nervous + excited? [MCQ: A) Bus was loud | B) She held backpack tightly | C) She smiled | D) She sang]', 'B', 1),
-((SELECT id FROM assessments WHERE item_code='5.RSLKID.1.A'),
-'[TF] Megan was worried she would not make friends.', 'T', 2),
-((SELECT id FROM assessments WHERE item_code='5.RSLKID.1.A'),
-'What can you infer? [MCQ: A) She is shy | B) She is outgoing | C) She is mean | D) She is angry]', 'B', 3),
-((SELECT id FROM assessments WHERE item_code='5.RSLKID.1.A'),
-'[TF] Megan did not enjoy her first day.', 'F', 4),
-((SELECT id FROM assessments WHERE item_code='5.RSLKID.1.A'),
-'Which shows friendship? [MCQ: A) They fought | B) They ignored each other | C) They shared snacks and talked | D) They left early]', 'C', 5);
->>>>>>> 5ab6d86470e9249873a8b1f1b4dfdecdd344007d
 
 COMMIT;
